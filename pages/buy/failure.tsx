770c5e802ca897c4058a9742c8a19c6e93bcc492
basON <wch.basinski@gmail.com>
2023-01-07 02:11:48 +0100

    Add default, success and failed files

---
+++ pages/buy/failure.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayout from "../../components/layout/DashboardLayout";
+
+function failure() {
+  return <div>failure</div>;
+}
+failure.PageLayout = DashboardLayout;
+export default failure;
