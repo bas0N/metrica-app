c14cc0725bb9a2657a317a364d489f75c6d505b6
basON <wch.basinski@gmail.com>
2023-02-11 19:20:18 +0100

    Add rewrites to next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://18.197.106.181:3001/:path*",
+      },
+    ];
+  },
   reactStrictMode: true,
   swcMinify: false,
   images: {
@@ -15,6 +23,7 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+
   // publicRuntimeConfig: {
   //   NEXT_PUBLIC_BACKEND_URL: process.env.BACKEND_URL,
   //   NEXT_PUBLIC_APP_URL: process.env.APP_URL,
